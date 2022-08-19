f9ddeab86e185ccd83f27a205dfbfdb28d5b8926
KI-DG <kanfs2022@gmail.com>
2022-08-19 21:11:32 +0900

    Feat: 괄호검사

--- algorithm/02_Stack/4886_괄호검사/s1.py
+++ algorithm/02_Stack/4886_괄호검사/s1.py
@@ -9,14 +9,27 @@ for tc in range(1, t + 1):
     stack = []
     answer = 1
     for text in texts:
-        if text == '(' and text == '{':     # '('가 있으면 stack에 더해준다.
+        if text == '(' or text == '{':     # '('가 있으면 stack에 더해준다.
             stack.append(text)
-        else:
-            if not stack:  # stack이 비어 있으면 나와라
+
+        elif text == ')':
+            if not stack:       # ')' 가없으면 0 을 출력해주고 반복문을 나가준다.
+                answer = 0
+                break
+            elif stack[-1] != '(':    # stack 마지막 원소가 '('가 아니면 0을 출력해준다.
+                answer = 0
                 break
             stack.pop()
-    else:
-        if not stack:
-            answer = 0
+        elif text == '}':
+            if not stack:
+                answer = 0
+                break
+            elif stack[-1] != '{':      # stack 마지막 원소가 '{'가 아니면 0을 출력해준다.
+                answer = 0
+                break
+            stack.pop()
+
+    if stack:
+        answer = 0
 
     print(f"#{tc} {answer}")
